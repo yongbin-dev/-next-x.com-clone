839fbfd3bc6832b5c05250c31f3bddd06646bd6f
yongbin_home <dydqls5757@gmail.com>
2024-01-23 21:36:31 +0900

    섹션2 : Context API

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 import ZLogo from '../../../public/zlogo.png';
 import NavMenu from "@/app/(afterLogin)/_component/NavMenu";
 import LogoutButton from "@/app/(afterLogin)/_component/LogoutButton";
-// import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
-// import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
+import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
+import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
 
 export default function AfterLoginLayout({ children }: { children: ReactNode }) {
   return (
@@ -44,12 +44,12 @@ export default function AfterLoginLayout({ children }: { children: ReactNode }) 
                 <input type="search" />
               </form>
             </div>
-            {/* <TrendSection /> */}
+            <TrendSection />
             <div className={style.followRecommend}>
               <h3>팔로우 추천</h3>
-              {/* <FollowRecommend />
               <FollowRecommend />
-              <FollowRecommend /> */}
+              <FollowRecommend />
+              <FollowRecommend />
             </div>
           </section>
         </div>
